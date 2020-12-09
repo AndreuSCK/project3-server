24d1e4b1cb12d26debdbc8043b8aa2d649d02c8a
Andreu <andreu.sck@gmail.com>
2020-12-09 02:15:07 +0100

    public updated

---
+++ public/precache-manifest.3c830dc9f88d7f867ecff13074d8208d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d21331610a971e90ad21dd9d4c8df16c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "775ce2fe4d58565c75a7",
+    "url": "/static/css/main.49cab5e4.chunk.css"
+  },
+  {
+    "revision": "4c42b7ff7ec61cb4e30b",
+    "url": "/static/js/2.a5cbc7ab.chunk.js"
+  },
+  {
+    "revision": "e758788412e5b10a746020105d83bc3c",
+    "url": "/static/js/2.a5cbc7ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "775ce2fe4d58565c75a7",
+    "url": "/static/js/main.7df3afa5.chunk.js"
+  },
+  {
+    "revision": "5179ff928fc7dafdbfe9",
+    "url": "/static/js/runtime-main.81fc7dff.js"
+  }
+]);
